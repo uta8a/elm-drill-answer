5d8a7f41e771ba8a8770a711b24a0ee6d17a7c9f
Masanori Tani <metro@uta8a.org>
2022-07-30 05:17:27 +0000

    feat: Q2

--- elm-drill/src/Q2_Booleans.elm
+++ elm-drill/src/Q2_Booleans.elm
@@ -18,67 +18,67 @@ module Q2_Booleans exposing (..)
 -}
 alwaysTrue : a -> Bool
 alwaysTrue _ =
-    Debug.todo "TODO"
+    True
 
 
 {-| 演算子を使って両方とも True の時 True を返します
 -}
 and : Bool -> Bool -> Bool
 and a b =
-    Debug.todo "TODO"
+    a && b
 
 
 {-| 演算子を使ってどちらかが True の時 True を返します
 -}
 or : Bool -> Bool -> Bool
 or a b =
-    Debug.todo "TODO"
+    a || b
 
 
 {-| 0 より大きければ True を返します
 -}
 isPositive : Int -> Bool
 isPositive n =
-    Debug.todo "TODO"
+    n > 0
 
 
 {-| 0 より小さければ True を返します
 -}
 isNegative : Int -> Bool
 isNegative n =
-    Debug.todo "TODO"
+    n < 0
 
 
 {-| 引数がちょうど 42 なら True を返します
 -}
 is42 : Int -> Bool
 is42 n =
-    Debug.todo "TODO"
+    n == 42
 
 
 {-| 引数が 42 でない時 True を返します
 -}
 isNot42 : Int -> Bool
 isNot42 n =
-    Debug.todo "TODO"
+    n /= 42
 
 
 {-| 偶数ならば True を返します
 -}
 isEven : Int -> Bool
 isEven n =
-    Debug.todo "TODO"
+    modBy 2 n == 0
 
 
 {-| 両方とも偶数ならば True を返します
 -}
 areEven : Int -> Int -> Bool
 areEven a b =
-    Debug.todo "TODO"
+    modBy 2 a == 0 && modBy 2 b == 0
 
 
 {-| 0 より大きい偶数ならば True を返します
 -}
 isPositiveAndEven : Int -> Bool
 isPositiveAndEven n =
-    Debug.todo "TODO"
+    n > 0 && modBy 2 n == 0
